2f46644e7dba4ca71f4237b21971c2a6782de384
skupadhyay024 <connect.shashik@gmail.com>
2019-06-25 22:04:07 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
+import { CustomerListComponent } from './components/customer-list/customer-list.component';
+import { CustomerComponent } from './components/customer/customer.component'
 
+//Services
+import { CustomerService } from './services/customer.service';
+import { OrderByPipe } from './pipes/order-by.pipe';
+import { SearchPipe } from './pipes/search.pipe';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerListComponent,
+    NavComponent,
+    OrderByPipe,
+    SearchPipe,
+    CustomerComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: "", redirectTo: "home", pathMatch: 'full' },
+      { path: "home", component: HomeComponent },
+      { path: "customers", component: CustomerListComponent },
+      { path: "customers/add", component: CustomerComponent },
+      { path: "customers/edit/:id", component: CustomerComponent },
+      { path: '**', component: HomeComponent }
+    ])
   ],
-  providers: [],
+  providers: [CustomerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
